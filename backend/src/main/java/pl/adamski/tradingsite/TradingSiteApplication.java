cbc8b29a0fa179760df449cc18bf0b74f8c0c6ad
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-04-02 16:49:56 +0200

    create project

---
+++ backend/src/main/java/pl/adamski/tradingsite/TradingSiteApplication.java
@@ -0,0 +1,13 @@
+package pl.adamski.tradingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/adamski/tradingsite/TradingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adamski.tradingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
