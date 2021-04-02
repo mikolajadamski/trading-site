eeb1670c9deb8b2ee6090b4e8a26cf7180dad833
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-04-02 23:55:12 +0200

    add frontend

--- backend/src/main/java/pl/adamski/tradingsite/dao/ProductRepository.java
+++ backend/src/main/java/pl/adamski/tradingsite/dao/ProductRepository.java
@@ -2,8 +2,9 @@ package pl.adamski.tradingsite.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.adamski.tradingsite.entity.Product;
 
-
+@CrossOrigin()
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
---
+++ frontend/angular-trading-site/src/app/common/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+
+     sku: string = '';
+     name: string = '';
+     description: string = '';
+     unitPrice: number = 0.0;
+     imageUrl: string = '';
+     active: boolean = false;
+     unitsInStock: number = 0;
+     dateCreated: Date = new Date();
+     lastUpdated: Date = new Date();
+
+    constructor(){}
+
+
+}
