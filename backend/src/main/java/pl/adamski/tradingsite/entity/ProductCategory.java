2877f7839b46391ee76427f086a61d90cb0f8a40
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-04-02 17:46:59 +0200

    add: Category and Product entities

---
+++ backend/src/main/java/pl/adamski/tradingsite/entity/ProductCategory.java
@@ -0,0 +1,21 @@
+package pl.adamski.tradingsite.entity;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
