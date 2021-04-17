650aa456ee577115af3d269623f545b6c8445c7d
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-04-17 16:32:20 +0200

    add: placeholder photo, cross origin address

--- backend/src/main/java/pl/adamski/tradingsite/dao/ProductRepository.java
+++ backend/src/main/java/pl/adamski/tradingsite/dao/ProductRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.adamski.tradingsite.entity.Product;
 
-@CrossOrigin()
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
