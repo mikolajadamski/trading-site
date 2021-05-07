074471a56c7db0c5e395df4e266d8f82199778e4
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-05-07 15:42:24 +0200

    fix frontend tests

--- frontend/angular-trading-site/src/app/components/product-details/product-details.component.spec.ts
+++ frontend/angular-trading-site/src/app/components/product-details/product-details.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule, Routes } from '@angular/router';
+import { CartDetailsComponent } from '../cart-details/cart-details.component';
+import { ProductListComponent } from '../product-list/product-list.component';
 
 import { ProductDetailsComponent } from './product-details.component';
 
@@ -6,8 +10,21 @@ describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
   let fixture: ComponentFixture<ProductDetailsComponent>;
 
+  const routes: Routes = [
+    { path: 'cart-details', component: CartDetailsComponent},
+    { path: 'products/:id', component: ProductDetailsComponent },
+    { path: 'search/:keyword', component: ProductListComponent },
+    { path: 'category/:id', component: ProductListComponent },
+    { path: 'category', component: ProductListComponent },
+    { path: 'products', component: ProductListComponent },
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
+    { path: '**', redirectTo: '/products', pathMatch: 'full' },
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule,
+      RouterModule.forRoot(routes)],
       declarations: [ ProductDetailsComponent ]
     })
     .compileComponents();
--- frontend/angular-trading-site/src/app/components/product-list/product-list.component.spec.ts
+++ frontend/angular-trading-site/src/app/components/product-list/product-list.component.spec.ts
@@ -1,13 +1,30 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule, Routes } from '@angular/router';
+import { CartDetailsComponent } from '../cart-details/cart-details.component';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 import { ProductListComponent } from './product-list.component';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
   let fixture: ComponentFixture<ProductListComponent>;
+  const routes: Routes = [
+    { path: 'cart-details', component: CartDetailsComponent},
+    { path: 'products/:id', component: ProductDetailsComponent },
+    { path: 'search/:keyword', component: ProductListComponent },
+    { path: 'category/:id', component: ProductListComponent },
+    { path: 'category', component: ProductListComponent },
+    { path: 'products', component: ProductListComponent },
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
+    { path: '**', redirectTo: '/products', pathMatch: 'full' },
+  ];
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule,
+      RouterModule.forRoot(routes)],
       declarations: [ ProductListComponent ]
     })
     .compileComponents();
