d4e147c07a389ddffdf3021fca3c0e5f01c47eff
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-05-08 19:07:05 +0200

    testing CI: update test so they fail

--- backend/src/test/java/pl/adamski/tradingsite/TradingSiteApplicationTests.java
+++ backend/src/test/java/pl/adamski/tradingsite/TradingSiteApplicationTests.java
@@ -3,11 +3,18 @@ package pl.adamski.tradingsite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TradingSiteApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void AssertFalseTest(){
+		assertTrue(false);
 	}
 
 }
